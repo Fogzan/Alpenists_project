096a9c88694e61deb7d8b638b2b2523e2382f10c
lixxteq <rlv1zhakov.liv@gmail.com>
2023-10-06 06:41:12 +0300

    specified mysql env vars to work with docker

--- Alpenists_project/app/config.py
+++ Alpenists_project/app/config.py
@@ -1,5 +1,7 @@
 SECRET_KEY = 'e341e6698cb20dd889d040a9be7d5fc129cb06255f349bd6ea3f901afe8d61b4'
 
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2059_alpenists_project:Qwerty123@std-mysql.ist.mospolytech.ru/std_2059_alpenists_project'
+#SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2059_alpenists_project:Qwerty123@std-mysql.ist.mospolytech.ru/std_2059_alpenists_project'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
+
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:secret@mysql_db:3306/alpenists'
