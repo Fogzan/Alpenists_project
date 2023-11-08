e7a865a32742cdc6e6cf58db945730d6123b2c47
Fogzan <91115262+Fogzan@users.noreply.github.com>
2023-11-09 00:58:40 +0300

    Update config.py

--- Alpenists_project/app/config.py
+++ Alpenists_project/app/config.py
@@ -6,7 +6,7 @@ class CONFIG():
     DB_NAME = getenv('MYSQL_DATABASE')
     MYSQL_HOST = getenv("MYSQL_HOST" if ENV == "production" else "MYSQL_DEV_HOST")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{getenv("MYSQL_USER")}:{getenv("MYSQL_PASSWORD")}@{MYSQL_HOST}:3306/{DB_NAME}'
     MYSQL_ENGINE_URI = f'mysql+mysqlconnector://{getenv("MYSQL_USER")}:{getenv("MYSQL_PASSWORD")}@{MYSQL_HOST}:3306'
 
