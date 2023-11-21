bc1e0faff150e6d30792ccf5f19c82845e88b109
Fogzan <fogzan@yandex.ru>
2023-11-22 01:13:03 +0300

    add AutoTests v4

--- AutoTests/main.py
+++ AutoTests/main.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,33 +11,27 @@ from climbers import climbers
 from group import group
 from climbing import climbing
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.maximize_window()
-driver.implicitly_wait(5)
+def test_main():
+   options = webdriver.ChromeOptions()
+   options.add_experimental_option("excludeSwitches", ["enable-logging"])
+   driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.get("http://192.168.0.139:5000")
-# time.sleep(5)
+   driver.maximize_window()
+   driver.implicitly_wait(5)
+   
+   driver.get("http://192.168.0.139:5000")
 
-result_authorization = authorization(driver=driver)
-print(result_authorization)
-if not result_authorization:
-   sys.exit("Ошибка в регистрации и авторизации")
-result_mountains = mountains(driver=driver)
-print(result_mountains)
-if not result_mountains:
-   sys.exit("Ошибка при добавлении горы")
-result_climbers = climbers(driver=driver)
-print(result_climbers)
-if not result_climbers:
-   sys.exit("Ошибка при добавлении альпиниста")
-result_group = group(driver=driver)
-print(result_group)
-if not result_group:
-   sys.exit("Ошибка при создании группы")
-result_climbing = climbing(driver=driver)
-print(result_climbing)
-if not result_climbing:
-   sys.exit("Ошибка при добавлении восхождения")
+   result_authorization = authorization(driver=driver)
+   assert result_authorization == True, "Ошибка в регистрации и авторизации."
+   result_mountains = mountains(driver=driver)
+   assert result_mountains == True, "Ошибка при добавлении горы."
+   result_climbers = climbers(driver=driver)
+   assert result_climbers == True, "Ошибка при добавлении альпиниста."
+   result_group = group(driver=driver)
+   assert result_group == True, "Ошибка при создании группы."
+   result_climbing = climbing(driver=driver)
+   assert result_climbing == True, "Ошибка при добавлении восхождения."
+
+   driver.close()
+   driver.quit()
