0e22cf7d041a58d99824581b5cc667ecd143c277
Fogzan <fogzan@yandex.ru>
2023-11-21 22:01:43 +0300

    add AutoTests v3

--- AutoTests/main.py
+++ AutoTests/main.py
@@ -21,18 +21,22 @@ driver.get("http://192.168.0.139:5000")
 # time.sleep(5)
 
 result_authorization = authorization(driver=driver)
-result_mountains = mountains(driver=driver)
-result_climbers = climbers(driver=driver)
-result_group = group(driver=driver)
-result_climbing = climbing(driver=driver)
 print(result_authorization)
-print(result_mountains)
-print(result_climbers)
-print(result_group)
-print(result_climbing)
-
-
 if not result_authorization:
-   sys.exit("some error message")
-
-   
+   sys.exit("Ошибка в регистрации и авторизации")
+result_mountains = mountains(driver=driver)
+print(result_mountains)
+if not result_mountains:
+   sys.exit("Ошибка при добавлении горы")
+result_climbers = climbers(driver=driver)
+print(result_climbers)
+if not result_climbers:
+   sys.exit("Ошибка при добавлении альпиниста")
+result_group = group(driver=driver)
+print(result_group)
+if not result_group:
+   sys.exit("Ошибка при создании группы")
+result_climbing = climbing(driver=driver)
+print(result_climbing)
+if not result_climbing:
+   sys.exit("Ошибка при добавлении восхождения")
