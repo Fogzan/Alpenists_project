512d6b655e0ad6d383a0fb54b8ce6188c589adf7
Fogzan <91115262+Fogzan@users.noreply.github.com>
2023-11-23 17:00:07 +0300

    Update main.py

--- AutoTests/main.py
+++ AutoTests/main.py
@@ -18,7 +18,7 @@ def test_main():
    options.add_argument('--headless')
    options.add_argument('--no-sandbox')
    options.add_argument('--disable-dev-shm-usage')
-   options.add_argument('--disable-extensions')
+  # options.add_argument('--disable-extensions')
 
   # options.add_argument('--disable-gpu')
   # options.add_argument('--remote-debugging-port=9222')
