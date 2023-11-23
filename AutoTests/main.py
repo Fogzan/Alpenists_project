bb2a7254cc6814794e7d3c46734a0a2ac0f7cba8
Fogzan <91115262+Fogzan@users.noreply.github.com>
2023-11-23 17:02:21 +0300

    Update main.py

--- AutoTests/main.py
+++ AutoTests/main.py
@@ -17,7 +17,7 @@ def test_main():
    options.add_experimental_option("excludeSwitches", ["enable-logging"])
    options.add_argument('--headless')
    options.add_argument('--no-sandbox')
-   options.add_argument('--disable-dev-shm-usage')
+  # options.add_argument('--disable-dev-shm-usage')
   # options.add_argument('--disable-extensions')
 
   # options.add_argument('--disable-gpu')
