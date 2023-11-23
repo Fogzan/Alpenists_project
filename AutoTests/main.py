c8bd4cd183f0c64e23af92c694bc04e0b0298a17
Fogzan <91115262+Fogzan@users.noreply.github.com>
2023-11-23 17:04:30 +0300

    Update main.py

--- AutoTests/main.py
+++ AutoTests/main.py
@@ -16,7 +16,7 @@ def test_main():
    options = webdriver.ChromeOptions()
    options.add_experimental_option("excludeSwitches", ["enable-logging"])
    options.add_argument('--headless')
-   options.add_argument('--no-sandbox')
+  # options.add_argument('--no-sandbox')
   # options.add_argument('--disable-dev-shm-usage')
   # options.add_argument('--disable-extensions')
 
